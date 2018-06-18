46b242dd616517500a1b2e4dccaa3b6cb69b2ed2
KasperSkytte <knaldhat@gmail.com>
2018-06-18 15:22:50 +0200

    added comments and reuse name of the sampleID column

--- R/amp_mergereplicates.R
+++ R/amp_mergereplicates.R
@@ -7,6 +7,7 @@
 #'
 #' @return An object of class \code{ampvis2}
 #'
+#' @importFrom magrittr %>%
 #' @importFrom rlang enquo
 #' @importFrom dplyr group_by summarise_at bind_rows
 #' @importFrom tibble column_to_rownames
@@ -27,32 +28,41 @@ amp_mergereplicates <- function(data,
                                 merge_var,
                                 merge_fun = "mean",
                                 round = "up") {
+  ### Data must be in ampvis2 format
+  if(class(data) != "ampvis2")
+    stop("The provided data is not in ampvis2 format. Use amp_load() to load your data before using ampvis functions. (Or class(data) <- \"ampvis2\", if you know what you are doing.)", call. = FALSE)
+  ### merge_var must be a length 1 string
   if(!is.character(merge_var) || length(merge_var) != 1)
     stop("\"merge_var\" must be a character string of length 1", call. = FALSE)
+  #capture merge function
   merge_fun <- rlang::enquo(merge_fun)
+  #find unique group names excluding than NA and empty strings ("")
   groups <- data$metadata[which(data$metadata[,merge_var] != ""), merge_var] %>%
     na.omit() %>%
     unique()
+  #retain name of first column
+  nameoffirstcol <- colnames(data$metadata)[1]
+  #add a group column to the abundance table to define the groups
   tempabund <- data$metadata[,c(1,which(colnames(data$metadata) == merge_var))] %>%
     merge(as.data.frame(t(data$abund)), by.x = 1, by.y = 0) %>% {
       .[,1] <- ifelse(.[,2] %in% groups, .[,2], .[,1])
-      colnames(.)[1] <- "SampleID"
+      colnames(.)[1] <- nameoffirstcol
       return(.[,-2])
     }
-
+  #merge samples based on the groups
   abund_merged <- tempabund[which(tempabund[,1] %in% groups),] %>%
-    dplyr::group_by(SampleID) %>%
+    dplyr::group_by(!!! syms(nameoffirstcol)) %>%
     dplyr::summarise_at(.vars = 2:ncol(.), .funs = merge_fun)
-
+  #combine the merged samples and unmerged samples if any
   if(any(!data$metadata[,merge_var] %in% groups)) {
     newabund <- dplyr::bind_rows(abund_merged, tempabund[which(!tempabund[,1] %in% groups),])
   } else
     newabund <- abund_merged
-
+  #transpose the new abundance table, remove first column and round up or down
   out <- data
   out$abund <- newabund %>%
     as.data.frame() %>%
-    tibble::column_to_rownames("SampleID") %>%
+    tibble::column_to_rownames(nameoffirstcol) %>%
     t() %>% {
       if(round == "up") {
         return(ceiling(.))
@@ -61,6 +71,7 @@ amp_mergereplicates <- function(data,
       } else
         return(.)
     }
+  #make new metadata keeping only the first row of each sample group
   out$metadata <- out$metadata[!duplicated(out$metadata$group),]
   out$metadata[,1] <- colnames(out$abund)
   rownames(out$metadata) <- out$metadata[,1]
