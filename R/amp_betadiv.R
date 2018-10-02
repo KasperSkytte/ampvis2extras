3345069962c897a2bdbc41d9d517637f2028e0a4
KasperSkytte <knaldhat@gmail.com>
2018-10-02 15:53:21 +0200

    styled sourcefiles

--- R/amp_betadiv.R
+++ R/amp_betadiv.R
@@ -21,37 +21,44 @@ amp_betadiv <- function(data,
                         values_size = 3) {
   distcoeffs <- as.matrix(vegan::vegdist(t(data$abund), method = distmeasure))
   distcoeffs[upper.tri(distcoeffs)] <- NA
-  if(!is.null(label_by)) {
-    labels <- apply(data$metadata[,c(1, which(colnames(data$metadata) %in% label_by))], 1, paste0, collapse = "; ")
+  if (!is.null(label_by)) {
+    labels <- apply(data$metadata[, c(1, which(colnames(data$metadata) %in% label_by))], 1, paste0, collapse = "; ")
   } else if (is.null(label_by)) {
     labels <- rownames(distcoeffs)
   }
   distcoeffs <- data.frame(label = labels, distcoeffs, check.names = FALSE)
   levels <- as.factor(distcoeffs[["label"]])
   distcoeffs <- tidyr::gather(distcoeffs,
-                              key = "y",
-                              value = "coeff",
-                              -label,
-                              na.rm = TRUE,
-                              factor_key = TRUE)
+    key = "y",
+    value = "coeff",
+    -label,
+    na.rm = TRUE,
+    factor_key = TRUE
+  )
   distcoeffs$label <- factor(distcoeffs$label, levels = levels)
 
-  p <- ggplot(distcoeffs,
-              aes(x = label,
-                  y = y,
-                  fill = coeff,
-                  label = round(1-coeff, 2))) +
+  p <- ggplot(
+    distcoeffs,
+    aes(
+      x = label,
+      y = y,
+      fill = coeff,
+      label = round(1 - coeff, 2)
+    )
+  ) +
     geom_tile(stat = "identity") +
     xlab("") +
     ylab("") +
     theme_classic() +
-    theme(axis.text.x = element_text(angle = 90,
-                                     vjust = 0.5,
-                                     hjust = 1)) +
+    theme(axis.text.x = element_text(
+      angle = 90,
+      vjust = 0.5,
+      hjust = 1
+    )) +
     scale_x_discrete(labels = levels) +
     scale_y_discrete(labels = levels) +
     scale_fill_continuous(low = "White", high = "red")
-  if(isTRUE(show_values)) {
+  if (isTRUE(show_values)) {
     p <- p +
       geom_text(size = values_size) +
       theme(legend.position = "none")
--- R/amp_mergereplicates.R
+++ R/amp_mergereplicates.R
@@ -20,61 +20,68 @@
 #' d$metadata$group <- c("group1", "group1", "group2", "group2", "group2", "group3", "group4", "group4")
 #' d$metadata
 #' dmerged <- amp_mergereplicates(d,
-#'                                merge_var = "group",
-#'                                merge_fun = "mean",
-#'                                round = "up")
+#'   merge_var = "group",
+#'   merge_fun = "mean",
+#'   round = "up"
+#' )
 #' dmerged
 amp_mergereplicates <- function(data,
                                 merge_var,
                                 merge_fun = "mean",
                                 round = "up") {
   ### Data must be in ampvis2 format
-  if(class(data) != "ampvis2")
+  if (class(data) != "ampvis2") {
     stop("The provided data is not in ampvis2 format. Use amp_load() to load your data before using ampvis functions. (Or class(data) <- \"ampvis2\", if you know what you are doing.)", call. = FALSE)
+  }
   ### merge_var must be a length 1 string
-  if(!is.character(merge_var) || length(merge_var) != 1)
+  if (!is.character(merge_var) || length(merge_var) != 1) {
     stop("\"merge_var\" must be a character string of length 1", call. = FALSE)
-  #capture merge function
+  }
+  # capture merge function
   merge_fun <- rlang::enquo(merge_fun)
-  #find unique group names excluding than NA and empty strings ("")
-  groups <- data$metadata[which(data$metadata[,merge_var] != ""), merge_var] %>%
+  # find unique group names excluding than NA and empty strings ("")
+  groups <- data$metadata[which(data$metadata[, merge_var] != ""), merge_var] %>%
     na.omit() %>%
     unique()
-  #retain name of first column
+  # retain name of first column
   nameoffirstcol <- colnames(data$metadata)[1]
-  #add a group column to the abundance table to define the groups
-  tempabund <- data$metadata[,c(1,which(colnames(data$metadata) == merge_var))] %>%
-    merge(as.data.frame(t(data$abund)), by.x = 1, by.y = 0) %>% {
-      .[,1] <- ifelse(.[,2] %in% groups, .[,2], .[,1])
+  # add a group column to the abundance table to define the groups
+  tempabund <- data$metadata[, c(1, which(colnames(data$metadata) == merge_var))] %>%
+    merge(as.data.frame(t(data$abund)), by.x = 1, by.y = 0) %>%
+    {
+      .[, 1] <- ifelse(.[, 2] %in% groups, .[, 2], .[, 1])
       colnames(.)[1] <- nameoffirstcol
-      return(.[,-2])
+      return(.[, -2])
     }
-  #merge samples based on the groups
-  abund_merged <- tempabund[which(tempabund[,1] %in% groups),] %>%
-    dplyr::group_by(!!! syms(nameoffirstcol)) %>%
+  # merge samples based on the groups
+  abund_merged <- tempabund[which(tempabund[, 1] %in% groups), ] %>%
+    dplyr::group_by(!!!syms(nameoffirstcol)) %>%
     dplyr::summarise_at(.vars = 2:ncol(.), .funs = merge_fun)
-  #combine the merged samples and unmerged samples if any
-  if(any(!data$metadata[,merge_var] %in% groups)) {
-    newabund <- dplyr::bind_rows(abund_merged, tempabund[which(!tempabund[,1] %in% groups),])
-  } else
+  # combine the merged samples and unmerged samples if any
+  if (any(!data$metadata[, merge_var] %in% groups)) {
+    newabund <- dplyr::bind_rows(abund_merged, tempabund[which(!tempabund[, 1] %in% groups), ])
+  } else {
     newabund <- abund_merged
-  #transpose the new abundance table, remove first column and round up or down
+  }
+  # transpose the new abundance table, remove first column and round up or down
   out <- data
   out$abund <- newabund %>%
     as.data.frame() %>%
     tibble::column_to_rownames(nameoffirstcol) %>%
-    t() %>% {
-      if(round == "up") {
+    t() %>%
+    {
+      if (round == "up") {
         return(ceiling(.))
-      } else if(round == "down") {
+      } else if (round == "down") {
         return(floor(.))
-      } else
+      } else {
         return(.)
+      }
     } %>%
     as.data.frame()
-  #make new metadata keeping only the first row of each sample group
-  out$metadata <- out$metadata[!duplicated(out$metadata$group),]
-  out$metadata[,1] <- colnames(out$abund)
-  rownames(out$metadata) <- out$metadata[,1]
+  # make new metadata keeping only the first row of each sample group
+  out$metadata <- out$metadata[!duplicated(out$metadata$group), ]
+  out$metadata[, 1] <- colnames(out$abund)
+  rownames(out$metadata) <- out$metadata[, 1]
   return(out)
 }
